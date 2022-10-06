b4587bbc5627a0d8d82146fa6cfbf24b520c333b
Mayara Lima <mayarademiranda89@gmail.com>
2022-10-06 11:32:46 +0100

    commit pc

---
+++ pc.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class pc {
+    String marca;
+    String modelo;
+    int ram;
+    int hd;
+
+    public pc(String marca1, String modelo1, int ram1, int hd2) {
+        this.marca = marca1;
+        this.modelo = modelo1;
+        this.ram = ram1;
+        this.hd = hd1;
+
+    }
+
+    //public PC(String modelo1. int ram1, int hd1) {
+    //this.marca = "Lenovo";
+    //this.modelo = "modelo1";
+    //this.ram = ram11;
+    //this.hd = hd1;
+
+
+    }
